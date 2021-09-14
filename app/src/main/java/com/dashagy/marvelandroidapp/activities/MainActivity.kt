2c1cbdd9d1eb789a335fffd9ea425ceb3e7882e1
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-09-14 18:01:49 -0300

    Proyect's base structure

--- app/src/main/java/com/dashagy/marvelandroidapp/activities/MainActivity.kt
+++ app/src/main/java/com/dashagy/marvelandroidapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dashagy.marvelandroidapp
+package com.dashagy.marvelandroidapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dashagy.marvelandroidapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
