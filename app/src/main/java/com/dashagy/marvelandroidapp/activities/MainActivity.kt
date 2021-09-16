0cbb47965840e6dc0c1bda2f5fe7ece742872daf
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-09-16 14:18:23 -0300

    Main Activity viewbinding

--- app/src/main/java/com/dashagy/marvelandroidapp/activities/MainActivity.kt
+++ app/src/main/java/com/dashagy/marvelandroidapp/activities/MainActivity.kt
@@ -2,11 +2,19 @@ package com.dashagy.marvelandroidapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dashagy.marvelandroidapp.R
+import com.dashagy.marvelandroidapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text="HolaMundo"
     }
 }
