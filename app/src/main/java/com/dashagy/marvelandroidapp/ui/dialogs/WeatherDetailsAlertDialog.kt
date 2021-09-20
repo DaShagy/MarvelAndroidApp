b78ddbc2c1a10944e515e5f5691a27439bdd9f18
Juan Jose Abuin <juanj.abuin@gmail.com>
2021-09-20 17:24:25 -0300

    Display characters description and image in alert dialog on item click

--- app/src/main/java/com/dashagy/marvelandroidapp/ui/dialogs/WeatherDetailsAlertDialog.kt
+++ app/src/main/java/com/dashagy/marvelandroidapp/ui/dialogs/WeatherDetailsAlertDialog.kt
@@ -4,20 +4,27 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import coil.load
 import com.dashagy.domain.entities.MarvelCharacter
+import com.dashagy.marvelandroidapp.databinding.AlertDialogCharacterDetailsBinding
 
 class CharacterDetailsAlertDialog (private val character: MarvelCharacter) : DialogFragment() {
 
+    private var _binding: AlertDialogCharacterDetailsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let{
+            val inflater = requireActivity().layoutInflater
+            _binding = AlertDialogCharacterDetailsBinding.inflate(inflater)
+
+            binding.characterDescription.text = character.description
+            binding.characterImage.load(character.image)
+
             val builder = AlertDialog.Builder(it)
 
-            val title = character.name
-
-            val message = character.description
-
-            builder.setTitle(title)
-                .setMessage(message)
+            builder.setTitle(character.name)
+                .setView(binding.root)
                 .create()
 
         } ?: throw IllegalStateException("Activity cannot be null")
